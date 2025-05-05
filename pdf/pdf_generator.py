e9e3a93aa4f89648df0b365c1c9882409217db36
Asaturno <asaturnoa36@gmail.com>
2025-05-04 22:00:32 -0600

    editor Quill

--- pdf/pdf_generator.py
+++ pdf/pdf_generator.py
@@ -1,33 +1,61 @@
 import webview
 
 def abrir_editor(html_contenido):
-    # Contenido HTML con TinyMCE 
+    # Escapar el contenido para insertar en JavaScript
+    html_contenido = html_contenido.replace("\\", "\\\\").replace("`", "'").replace("\n", "").replace('"', '\\"')
+
     html_editor = f"""
     <!DOCTYPE html>
     <html>
     <head>
-      <meta charset="UTF-8">
-      <script src="https://cdn.tiny.cloud/1/nj7cl63s6yjhu7aa6mkc0y0yjph4725hnk5ia2lrwlvrctas/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>
-      <script>
-        let initial_content = `{html_contenido.replace("`", "'")}`;
-        tinymce.init({{
-          selector: '#editor',
-          height: 600,
-          menubar: false,
-          plugins: 'lists link image table code',
-          toolbar: 'undo redo | styles | bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | code',
-          setup: function (editor) {{
-            editor.on('init', function () {{
-              editor.setContent(initial_content);
-            }});
-          }}
-        }});
-      </script>
+      <meta charset="utf-8">
+      <title>Editor de Constancia</title>
+      <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
+      <script src="https://cdn.quilljs.com/1.3.6/quill.min.js"></script>
+      <style>
+        body {{
+          margin: 0;
+          padding: 20px;
+          font-family: sans-serif;
+        }}
+        #editor-container {{
+          height: 500px;
+          background: #fff;
+        }}
+      </style>
     </head>
     <body>
-      <textarea id="editor"></textarea>
+      <div id="editor-container"></div>
+
+      <script>
+        var quill = new Quill('#editor-container', {{
+          theme: 'snow',
+          modules: {{
+            toolbar: [
+              ['bold', 'italic', 'underline'],
+              ['link', 'image'],
+              [{{
+                'list': 'ordered'
+              }}, {{
+                'list': 'bullet'
+              }}],
+              ['clean']
+            ]
+          }}
+        }});
+
+        // Insertar contenido HTML inicial
+        const html = "{html_contenido}";
+        const temp = document.createElement('div');
+        temp.innerHTML = html;
+        quill.setContents(quill.clipboard.convert(temp.innerHTML), 'silent');
+
+        
+      </script>
     </body>
     </html>
     """
+#// Futuro: podríamos obtener el contenido con quill.root.innerHTML o quill.getContents()
     webview.create_window("Editor de Constancia", html=html_editor, width=900, height=700)
     webview.start()
+
